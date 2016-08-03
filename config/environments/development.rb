661756a0db0852bca84578650fa33afc33fce53a
Shweta <shwetasingh0777@gmail.com>
2016-08-03 09:57:29 +0530

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,8 +45,8 @@ config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                  587,
   domain:               'localhost:3000',
-  user_name:            '1519shwetasingh@gmail.com',
-  password:             'Ruby@321',
+  user_name:            'someemail.com',
+  password:             'password',
   authentication:       'plain',
   enable_starttls_auto:  true  
   }
